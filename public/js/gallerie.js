04f64b0e63f76bfabc91153c9a7c95e4775044e0
JeremyLarcher <larcherjeremy@hotmail.fr>
2023-09-28 12:21:06 +0200

    ajout des logos de réseaux, amélioration du CSS, script js pour galerie photo

---
+++ public/js/gallerie.js
@@ -0,0 +1,23 @@
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+prevButton.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+});
+
+nextButton.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+});
+
+// Afficher la première photo au chargement de la page
+showSlide(currentSlide);
