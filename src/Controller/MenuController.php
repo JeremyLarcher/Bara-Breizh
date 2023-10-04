73b519c67c19875dc47bb8e2492e76e556a08b2a
JeremyLarcher <larcherjeremy@hotmail.fr>
2023-10-04 12:54:41 +0200

    amélioration du responsive, ajout du téléchargement du menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MenuController extends AbstractController
@@ -14,5 +16,23 @@ class MenuController extends AbstractController
         return $this->render('menu/afficher.html.twig', [
             'controller_name' => 'MenuController',
         ]);
+    }
+
+    #[Route('/telecharger', name: 'menu_telecharger')]
+    public function telecharger(): BinaryFileResponse
+    {
+        $pdfPath = $this->getParameter('kernel.project_dir') . '/public/img/menu/menu.pdf'; // Chemin vers le fichier PDF
+
+        // Créez une réponse binaire pour le fichier PDF
+        $response = new BinaryFileResponse($pdfPath);
+
+        // Définissez les en-têtes de réponse pour forcer le téléchargement du fichier
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            'menu.pdf' // Nom du fichier téléchargé
+        ));
+
+        return $response;
     }
 }
