3cabfa90a12201063e8a7b959b18105c13d21e6d
JeremyLarcher <larcherjeremy@hotmail.fr>
2023-10-05 12:00:48 +0200

    page administrateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
+use App\Entity\Utilisateur;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +13,47 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+
+
+        // Récupérer le nombre total d'utilisateur
+        $totalUsers = $entityManager->getRepository(Utilisateur::class)->count([]);
+        // Récupérer le nombre total de commentaires
+        $totalComments = $entityManager->getRepository(Commentaire::class)->count([]);
+
+        // Récupérer la moyenne des notes
+        $avgRating = $entityManager->getRepository(Commentaire::class)->createQueryBuilder('c')
+            ->select('AVG(c.note) as averageRating')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $formattedAvgRating = number_format($avgRating, 1);
+
+        // Récupérer les regions
+        $query = $entityManager->createQuery(
+            'SELECT r.nom AS region, COUNT(u.id) AS nombreUtilisateurs
+            FROM App\Entity\Region r
+            JOIN App\Entity\Utilisateur u WITH r.id = u.region
+            GROUP BY r.id
+            ORDER BY nombreUtilisateurs DESC'
+        );
+
+        $resultats = $query->getResult();
+
+        // La région la plus fréquemment utilisée est la première dans le résultat
+        $regionLaPlusFrequente = $resultats[0]['region'];
+        $nombreUtilisateurs = $resultats[0]['nombreUtilisateurs'];
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'totalUsers' => $totalUsers,
+            'totalComments' => $totalComments,
+            'averageRating' => $formattedAvgRating,
+            'regionLaPlusFrequente' => $regionLaPlusFrequente,
+            'nombreUtilisateurs' => $nombreUtilisateurs,
         ]);
+
+
     }
 }
